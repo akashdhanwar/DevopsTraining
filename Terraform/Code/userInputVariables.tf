f3147ba3423d7a4d68de96dc8c1acfa34b2783f6
Akash Dhanwar <akash.dhanwar112@gmail.com>
2023-02-08 22:42:12 +0530

    Updates

--- Terraform/Code/userInputVariables.tf
+++ Terraform/Code/userInputVariables.tf
@@ -1,5 +1,3 @@
-#Pass variable value from commandLine  -   For integrating with tools like Jenkins
-#                                          "terraform plan -var "username=Akash"
 
 output "printname" {
   value = "Hello ${var.userName} your age is ${var.age}"
--- Terraform/Code/variables.tf
+++ Terraform/Code/variables.tf
@@ -1,7 +1,10 @@
-# Input Variables            -   Created by using "variable" block. Example - variable username{}
-#                                Accessed in "output" block by "${var.username}"
-# Multiple variables         -   Default value - variable username{ default: "Akash"}
-#                                Terraform asks for input if it is not set
+# Input Variables                       -   Created by using "variable" block. Example - variable username{}
+#                                           Accessed in "output" block by "${var.username}"
+#                                           Default value - variable username{ default: "Akash"}
+#                                           Terraform asks for input if it is not set
+# Pass variable value from commandLine  -   For integrating with tools like Jenkins
+#                                           terraform plan -var "username=Akash"
+#                                           terraform plan -var "userName=Akash" -var "age=12"
 
 variable "userName" {
   default = "DefaultValue"
