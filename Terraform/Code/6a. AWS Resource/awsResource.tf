a564f9e0a7310763bd3cc034cdd51ccf4bbe6ef3
Akash Dhanwar <akash.dhanwar112@gmail.com>
2023-02-26 12:19:13 +0530

    Updates

---
+++ Terraform/Code/6a. AWS Resource/awsResource.tf
@@ -0,0 +1,2 @@
+# AWS Instance                  -       Go to Management Console, Search EC2 Instance, Create Ubuntu VM Server
+#                                       AMI ID - ami-0557a15b87f6559cf
